c108bd92e9e928de8951ff5727365aa9059fff29
aecnetwork641-svg <aecnetwork641@gmail.com>
2026-09-26 03:45:24 +0500

    feat: remove Student Login button from navbar header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -240,9 +240,6 @@ export default function Navbar() {
 
         {/* Action CTAs */}
         <div className="hidden items-center gap-3 md:flex">
-          <Link href="/login" className="text-sm font-medium text-aec-navy/80 hover:text-aec-navy px-3 py-2">
-            Student Login
-          </Link>
           <Link href="/courses" className="btn-secondary text-xs px-4 py-2">
             Explore Courses
           </Link>
